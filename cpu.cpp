80346d15419b27475c5279426c06d9e903d4b970
rohitmathurs <mathur.9@iitj.ac.in>
2023-01-28 19:51:51 +0530

    Test bench and all code updated

--- cpu.cpp
+++ cpu.cpp
@@ -19,6 +19,7 @@ Added code marked with "M21AIE249"
 
 References:
 RISC-V Instructions: https://github.com/jameslzhu/riscv-card/blob/master/riscv-card.pdf
+RISC-V Instruction Encoder/Interpreter, LupLab @ University of California, Davis: https://luplab.gitlab.io/rvcodecjs/
 
 For simulating this module, follow the instructions in Readme.md
 
@@ -27,7 +28,7 @@ For simulating this module, follow the instructions in Readme.md
 #include "systemc.h"
 
 SC_MODULE (cpu) {
-  sc_in_clk     clock ;      // Clock input of the design
+  sc_in_clk clock;      // Clock input of the design
   sc_out<bool> cpu_out;
 
   sc_in <sc_bv<32>> instruction;
@@ -42,8 +43,6 @@ SC_MODULE (cpu) {
   // 32 CPU registers
   sc_int<32> register_memory[32];
 
-  //cout<<register_memory[0]<<"check 1\n";
-
     /*
     R-type: register-register
     I-type: short immediates and loads
@@ -52,6 +51,7 @@ SC_MODULE (cpu) {
     U-type: long immediates
     J-type: unconditional jumps, a variation of U-type
     */
+
 // M21AIE249: Corrected a few entries in original code:
 
   sc_bv<7>  R_type_opcode = "0110011";
@@ -92,6 +92,10 @@ SC_MODULE (cpu) {
   sc_bv<3>  B_type_blt_func3 = "100";
   sc_bv<3>  B_type_bge_func3 = "101";
   
+  // M21AIE249 added lw and lb instructions
+  sc_bv<3>  I_type_lw_func3 = "010";
+  sc_bv<3>  I_type_lb_func3 = "000";
+  
   //------------Code Starts Here-------------------------
   // Below function implements actual cpu logic
   
@@ -101,31 +105,17 @@ SC_MODULE (cpu) {
     // from the location pointed by Program Counter
     sc_bv<32> temp = instruction_memory.read()( (32*32) -1 - Program_Counter ,  (32*32) -32 - Program_Counter );
 
-    //for(int i = Program_Counter;i<Program_Counter+32)
-
-    //sc_bv<32> temp = instruction_memory.range(Program_Counter,Program_Counter+32);
-    //cout<<instruction_memory.read()(Program_Counter+31, Program_Counter )<<"\n";
-    
-    //cout<<"after  "<<(data_memory.read()( (32*32) -1 - (0)*32 ,  (32*32) -32 - (0)*32 ) )<<"\n";
-        
-    //return;
-    
     cout<<"\nProgram Counter :"<<Program_Counter<<"\n";
     Program_Counter.write(Program_Counter+4*8);
-    //  R-TYPE
-    cout<<"Current Instruction "<<temp<<"\n";
     
-    //"00000010000100000 100 000101001111"
-    //"00000010000100000 111 000101001111"
-    //"00000010000100000 110 000101001111"
-    //cout<<temp.range(6,0)<<" "<<temp.range(31,25)<<" "<<temp.range(14,12)<<"\n";
+    cout<<"Current Instruction "<<temp<<"\n";
+    cout<<"Data Memory Location: "<<(data_memory.read()( (32*32) -1 - (0)*32 ,  (32*32) -32 - (0 )*32 ) )<<"\n";
 
-    cout<<"Data Memory Location 0: "<<(data_memory.read()( (32*32) -1 - (0)*32 ,  (32*32) -32 - (0 )*32 ) )<<"\n";
-
+	//R-Type
     if(temp.range(6,0)==R_type_opcode) {
-
+	// Add
       if(temp.range(31,25)==R_type_add_func7 && temp.range(14,12)==R_type_add_func3){
-        cout<<"add\n";
+        cout<<"Add ";
         sc_int<32> loc_operand1  = (temp.range(24,20)).to_int();
         sc_int<32> loc_operand2  = (temp.range(19,15)).to_int();
         sc_int<32> loc_dest  = (temp.range(11,7)).to_int();
@@ -136,8 +126,9 @@ SC_MODULE (cpu) {
         
         cout<<"@" << sc_time_stamp() <<"\n";
       }
+      // Subtract
       else if(temp.range(31,25)==R_type_sub_func7 && temp.range(14,12)==R_type_sub_func3){
-
+      cout<<"Sub ";
       sc_int<32> loc_operand1  = (temp.range(24,20)).to_int();
       sc_int<32> loc_operand2  = (temp.range(19,15)).to_int();
       sc_int<32> loc_dest  = (temp.range(11,7)).to_int();
@@ -147,12 +138,10 @@ SC_MODULE (cpu) {
       cout<<register_memory[loc_dest]<<" "<<register_memory[loc_operand1]<<" "<<register_memory[loc_operand2]<<"\n";
       
       cout<<"@" << sc_time_stamp() <<"\n";
-
-
       }
-
+	// AND
       else if(temp.range(31,25)==R_type_and_func7 && temp.range(14,12)==R_type_and_func3){
-        cout<<"and\n";
+        cout<<"AND ";
         sc_int<32> loc_operand1  = (temp.range(24,20)).to_int();
         sc_int<32> loc_operand2  = (temp.range(19,15)).to_int();
         sc_int<32> loc_dest  = (temp.range(11,7)).to_int();
@@ -162,11 +151,10 @@ SC_MODULE (cpu) {
         cout<<register_memory[loc_dest]<<" "<<register_memory[loc_operand1]<<" "<<register_memory[loc_operand2]<<"\n";
         
         cout<<"@" << sc_time_stamp() <<"\n";
-
       }
-
+	// XOR
       else if(temp.range(31,25)==R_type_xor_func7 && temp.range(14,12)==R_type_xor_func3){
-        cout<<"xor\n";
+        cout<<"XOR ";
         sc_int<32> loc_operand1  = (temp.range(24,20)).to_int();
         sc_int<32> loc_operand2  = (temp.range(19,15)).to_int();
         sc_int<32> loc_dest  = (temp.range(11,7)).to_int();
@@ -176,10 +164,10 @@ SC_MODULE (cpu) {
         cout<<register_memory[loc_dest]<<" "<<register_memory[loc_operand1]<<" "<<register_memory[loc_operand2]<<"\n";
         
         cout<<"@" << sc_time_stamp() <<"\n";
-
       }
+      // OR
       else if(temp.range(31,25)==R_type_or_func7 && temp.range(14,12)==R_type_or_func3){
-        cout<<"or\n";
+        cout<<"OR ";
         sc_int<32> loc_operand1  = (temp.range(24,20)).to_int();
         sc_int<32> loc_operand2  = (temp.range(19,15)).to_int();
         sc_int<32> loc_dest  = (temp.range(11,7)).to_int();
@@ -192,7 +180,7 @@ SC_MODULE (cpu) {
       }
 	// M21AIE249: Handling sll
       else if(temp.range(31,25)==R_type_sll_func7 && temp.range(14,12)==R_type_sll_func3){
-        cout<<"sll\n";
+        cout<<"SLL ";
         sc_int<32> loc_operand1  = (temp.range(24,20)).to_int();
         sc_int<32> loc_operand2  = (temp.range(19,15)).to_int();
         sc_int<32> loc_dest  = (temp.range(11,7)).to_int();
@@ -205,7 +193,7 @@ SC_MODULE (cpu) {
       }
 	// M21AIE249: Handling srl 
       else if(temp.range(31,25)==R_type_srl_func7 && temp.range(14,12)==R_type_srl_func3){
-        cout<<"srl\n";
+        cout<<"SRL ";
         sc_int<32> loc_operand1  = (temp.range(24,20)).to_int();
         sc_int<32> loc_operand2  = (temp.range(19,15)).to_int();
         sc_int<32> loc_dest  = (temp.range(11,7)).to_int();
@@ -216,29 +204,40 @@ SC_MODULE (cpu) {
         
         cout<<"@" << sc_time_stamp() <<"\n";
       }
-
     }
-        
+     // I - type       
     else if(temp.range(6,0)==I_type_opcode){
-      // I - type
-
-      // implementing load
-        //cout<<"vksdjbvkjn\n";
-        cout<<"load\n";
+    // Implementing load word
+        cout<<"Load ";
         sc_int<32> offset   = (temp.range(31,20)).to_int();
         sc_int<32> loc_operand  = (temp.range(19,15)).to_int();
         sc_int<32> funct3  = (temp.range(14,12)).to_int();
         sc_int<32> loc_dest  = (temp.range(11,7)).to_int();
-        cout<<"Register at "<< loc_dest<<" value "<<register_memory[loc_dest]<<"\n";
-        register_memory[loc_dest] = (data_memory.read()( (32*32) -1 - (register_memory[loc_operand]+offset)*32 ,  (32*32) -32 - (register_memory[loc_operand]+offset )*32 ) ).to_int();    
-        cout<<(data_memory.read()( (32*32) -1 - (register_memory[loc_operand]+offset)*32 ,  (32*32) -32 - (register_memory[loc_operand]+offset )*32 ) )<<"\n";
-        cout<<"Register at "<< loc_dest<<" value "<<register_memory[loc_dest]<<"\n";
-
-        cout<<"@" << sc_time_stamp() <<"\n";
+        
+        if(funct3==I_type_lw_func3.to_int()){
+		cout<<"Register at "<< loc_dest<<" value "<<register_memory[loc_dest]<<"\n";
+		register_memory[loc_dest] = (data_memory.read()( (32*32) -1 - (register_memory[loc_operand]+offset)*32 ,  (32*32) -32 - (register_memory[loc_operand]+offset )*32 ) ).to_int();
+		    
+		cout<<(data_memory.read()( (32*32) -1 - (register_memory[loc_operand]+offset)*32 ,  (32*32) -32 - (register_memory[loc_operand]+offset )*32 ) )<<"\n";
+		
+		cout<<"Register at "<< loc_dest<<" value "<<register_memory[loc_dest]<<"\n";
+	        
+	        cout<<"@" << sc_time_stamp() <<"\n";
+	}
+	else if(funct3==I_type_lb_func3.to_int()){
+		cout<<"Register at "<< loc_dest<<" value "<<register_memory[loc_dest]<<"\n";
+		register_memory[loc_dest] = (data_memory.read()( (32*32) -1 - (register_memory[loc_operand]+offset)*32 ,  (32*32) -16 - (register_memory[loc_operand]+offset )*32 ) ).to_int();    
+		
+		cout<<(data_memory.read()( (32*32) -1 - (register_memory[loc_operand]+offset)*32 ,  (32*32) -16 - (register_memory[loc_operand]+offset )*32 ) )<<"\n";
+		
+		cout<<"Register at "<< loc_dest<<" value "<<register_memory[loc_dest]<<"\n";
+	        
+	        cout<<"@" << sc_time_stamp() <<"\n";
+	}        
     }
     else if(temp.range(6,0)==S_type_opcode){
-        
-        cout<<"store\n";
+       // Implementing store word      
+        cout<<"Store ";
         sc_bv<12> offset_bits;
         offset_bits.range(11,5) = temp.range(31,25);
         offset_bits.range(4,0) = temp.range(11,7);
@@ -251,14 +250,14 @@ SC_MODULE (cpu) {
         sc_bv<32*32> temp = data_memory;
         
         temp.range( (32*32) -1 - (register_memory[loc_base]+offset)*32 ,  (32*32) -32 - (register_memory[loc_base]+offset )*32 ) = register_memory[loc_src];
-        cout<<"storing "<<temp.range( (32*32) -1 - (register_memory[loc_base]+offset)*32 ,  (32*32) -32 - (register_memory[loc_base]+offset )*32 )<<"\n";
+        cout<<"Storing "<<temp.range( (32*32) -1 - (register_memory[loc_base]+offset)*32 ,  (32*32) -32 - (register_memory[loc_base]+offset )*32 )<<"\n";
         data_memory.write(temp.range((32*32) -1, 0));
         
         cout<<"@" << sc_time_stamp() <<"\n";
-
     }
+    // Branch
     else if(temp.range(6,0)==B_type_opcode){
-      cout<<"branch\n";
+      cout<<"Branch ";
       sc_bv<13> offset_bits;
       offset_bits.range(11,11) = temp.range(7,7);
       offset_bits.range(4,1) = temp.range(11,8);
@@ -267,40 +266,41 @@ SC_MODULE (cpu) {
       offset_bits.range(0,0) = "0";
 
       sc_int<32> funct3  = (temp.range(14,12)).to_int();
-
       sc_int<32> loc_operand1  = (temp.range(24,20)).to_int();
       sc_int<32> loc_operand2  = (temp.range(19,15)).to_int();
-      
+      cout<<register_memory[loc_operand1]<<" "<<register_memory[loc_operand2]<<"\n";
+      // Branch if not equal
       if(funct3==B_type_bne_func3.to_int()){
+        cout<<"BNE ";
         if(register_memory[loc_operand1]!=register_memory[loc_operand2] ){
           Program_Counter.write(Program_Counter + 2*32*offset_bits.to_int());
-          cout<<"bne\n";
         }
-      }  
+      }
+      // Branch if equal        
       else if(funct3==B_type_beq_func3.to_int()){
+        cout<<"BEQ ";
         if(register_memory[loc_operand1]==register_memory[loc_operand2] ){
           Program_Counter.write(Program_Counter + 2*32*offset_bits.to_int());
-          cout<<"beq\n";
-        }
+          }
       }
 	// M21AIE249: Handling blt  
       else if(funct3==B_type_blt_func3.to_int()){
+        cout<<"BLT ";
         if(register_memory[loc_operand1]<register_memory[loc_operand2] ){
           Program_Counter.write(Program_Counter + 2*32*offset_bits.to_int());
-          cout<<"blt\n";
         }
       }
 	// M21AIE249: Handling bge
       else if(funct3==B_type_bge_func3.to_int()){
+        cout<<"BGE ";
         if(register_memory[loc_operand1]>=register_memory[loc_operand2] ){
           Program_Counter.write(Program_Counter + 2*32*offset_bits.to_int());
-          cout<<"bge\n";
         }
       }
       cout<<"@" << sc_time_stamp() <<"\n";
     }
     else if(temp.range(6,0)==J_type_opcode){
-      cout<<"unconditional branch\n";
+      cout<<"Unconditional Branch\n";
       sc_bv<21> offset_bits;
       offset_bits.range(0,0) = "0";
       offset_bits.range(10,1) = temp.range(30,21);
@@ -311,11 +311,9 @@ SC_MODULE (cpu) {
       Program_Counter.write(Program_Counter + 2*32*offset_bits.to_int());
       cout<<"@" << sc_time_stamp() <<"\n";
     }
-
     else {
       cout<<"@" << sc_time_stamp()<<" "<<instruction <<" Null  \n";
-    }
-    
+    }    
   } // End of function select_line()
 
   // Constructor for the counter
@@ -324,17 +322,16 @@ SC_MODULE (cpu) {
   // edge of the clock and also when ever reset changes state
   
   SC_CTOR(cpu) {
-    register_memory[0] = 111;
-    register_memory[1] = 111;
+  // Load some registers with some values
+    register_memory[0] = 11;
+    register_memory[1] = 11;
     register_memory[2] = 0;
     register_memory[3] = 3;
-    register_memory[5] = 0;
+    register_memory[5] = 1;
 
-    cout<<"\nExecuting new"<<endl;
+    cout<<"\nExecuting...."<<endl;
     SC_METHOD(select_line);
     sensitive << clock.pos();
     //sensitive << reset; M21AIE249: Why should this be commented out?
-    
   } // End of Constructor
-
 }; // End of Module
